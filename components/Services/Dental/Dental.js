10d8b96adb3ade59edd6ea10f5648c83020927aa
atikur-24 <atikurrahman7223@gmail.com>
2024-05-20 00:53:11 +0600

    responsive dental services page

--- components/Services/Dental/Dental.js
+++ components/Services/Dental/Dental.js
@@ -7,14 +7,14 @@ import React from "react";
 const Dental = () => {
   return (
     <div className="tw-bg-primary tw-text-white">
-      <Container className="tw-py-28">
-        <div className="tw-flex tw-gap-x-[60px]">
+      <Container className="tw-py-10 md:tw-py-14 lg:tw-py-28">
+        <div className="tw-flex tw-flex-col tw-gap-x-[60px] tw-gap-y-10 lg:tw-flex-row">
           {/* description part */}
-          <div className="tw-w-1/2">
-            <h3 className="tw-mb-11 tw-text-[28px] tw-font-semibold tw-capitalize tw-leading-8 tw-text-secondary">
+          <div className="lg:tw-w-1/2">
+            <h3 className="tw-mb-5 tw-text-lg tw-font-medium tw-capitalize tw-text-secondary md:tw-text-xl lg:tw-mb-11 lg:tw-text-[28px] lg:tw-font-semibold lg:tw-leading-8">
               It’s Much More Than Fresh Breath
             </h3>
-            <div className="tw-space-y-5 tw-text-lg tw-font-light">
+            <div className="tw-space-y-2 tw-text-sm tw-font-light md:tw-space-y-3 lg:tw-space-y-5 lg:tw-text-lg">
               <p>
                 Regular dental care for your pet will do so much more than make
                 those kisses less pungent—although that is a nice side effect.
@@ -37,41 +37,30 @@ const Dental = () => {
             </div>
           </div>
           {/* dental services part */}
-          <div className="tw-w-1/2">
-            <h3 className="tw-mb-11 tw-text-[28px] tw-font-semibold tw-capitalize tw-leading-8 tw-text-secondary">
+          <div className="lg:tw-w-1/2">
+            <h3 className="tw-mb-4 tw-text-lg tw-font-medium tw-capitalize tw-text-secondary md:tw-text-xl lg:tw-mb-11 lg:tw-text-[28px] lg:tw-font-semibold lg:tw-leading-8">
               Dental Services:
             </h3>
-            <div className="tw-flex tw-flex-col tw-gap-6 tw-divide-x-0 tw-divide-y-2 tw-divide-solid tw-divide-gray-5 tw-text-secondary">
-              <div className="tw-flex tw-items-center tw-gap-x-3">
+            <div className="tw-flex tw-flex-col tw-gap-3 tw-divide-x-0 tw-divide-y tw-divide-solid tw-divide-gray-5 tw-text-secondary lg:tw-gap-6 lg:tw-divide-y-2">
+              <div className="tw-flex tw-items-center tw-gap-x-2 tw-text-base lg:tw-gap-x-3 lg:tw-text-2xl">
                 <div>
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faPaw}
-                    className="tw-rotate-45"
-                  />
+                  <FontAwesomeIcon icon={faPaw} className="tw-rotate-45" />
                 </div>
-                <span className="tw-text-2xl">Dental Cleanings</span>
+                <span>Dental Cleanings</span>
               </div>
-              <div className="tw-flex tw-items-center tw-gap-x-3 tw-pt-6">
+              <div className="tw-flex tw-items-center tw-gap-x-2 tw-pt-3 tw-text-base lg:tw-gap-x-3 lg:tw-pt-6 lg:tw-text-2xl">
                 <div>
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faPaw}
-                    className="tw-rotate-45"
-                  />
+                  <FontAwesomeIcon icon={faPaw} className="tw-rotate-45" />
                 </div>
-                <span className="tw-text-2xl">Tooth Extractions</span>
+                <span>Tooth Extractions</span>
               </div>
-              <div className="tw-flex tw-items-center tw-gap-x-3 tw-pt-6">
+              <div className="tw-flex tw-items-center tw-gap-x-2 tw-pt-3 tw-text-base lg:tw-gap-x-3 lg:tw-pt-6 lg:tw-text-2xl">
                 <div>
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faPaw}
-                    className="tw-rotate-45"
-                  />
+                  <FontAwesomeIcon icon={faPaw} className="tw-rotate-45" />
                 </div>
-                <span className="tw-text-2xl">Oral Surgery</span>
+                <span>Oral Surgery</span>
               </div>
+              <span></span>
             </div>
             {/* call part */}
             <CallSection />
