d23812423da21a4185705dab479bee18ea9549e8
atikur-24 <atikurrahman7223@gmail.com>
2024-05-30 23:09:34 +0600

    meet your doctor responsive

--- components/LandingPage/MeetDoctor.js
+++ components/LandingPage/MeetDoctor.js
@@ -7,36 +7,39 @@ import SectionTitle from "../UI/SectionTitle";
 
 function MeetDoctor() {
   return (
-    <Container className="d-none d-lg-block">
+    <Container className="">
       <div style={{ padding: "120px 0" }}>
         <Row
           style={{
-            borderRadius: "400px",
             overflow: "hidden",
           }}
-          className="pt-5 px-5 tw-bg-secondary"
+          className="pt-5 px-5 pb-5 pb-lg-0 tw-rounded-lg tw-bg-secondary md:tw-rounded-[400px]"
         >
           <Col
-            md={6}
+            lg={6}
             style={{
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
               columnGap: "1rem",
-              rowGap: "2rem",
             }}
+            className="align-items-center align-items-lg-start row-gap-2 row-gap-lg-4"
           >
             <SectionTitle className="tw-bg-tertiary">
               MEET YOUR DOCTOR
             </SectionTitle>
-            <p className="mb-0 tw-pt-1 tw-text-xl tw-font-medium tw-text-primary lg:tw-pt-2 lg:tw-text-4xl lg:tw-font-semibold">
+            <p className="mb-0 tw-pt-1 tw-text-center tw-text-xl tw-font-medium tw-text-primary lg:tw-pt-2 lg:tw-text-left lg:tw-text-4xl lg:tw-font-semibold">
               We’re All About Smiling Faces & Wagging Tails
             </p>
             <Link href="/book-now">
               <CustomButton>Book Now</CustomButton>
             </Link>
           </Col>
-          <Col md={6} style={{ maxWidth: "90%", overflow: "hidden" }}>
+          <Col
+            md={6}
+            style={{ maxWidth: "90%", overflow: "hidden" }}
+            className="d-none d-lg-block"
+          >
             <Image
               src="/assets/images/meet-doctor.png"
               width={450}
