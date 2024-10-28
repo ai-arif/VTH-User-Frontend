64c7efff9568bef82c8543708479b0a2ad84bba5
atikur-24 <atikurrahman7223@gmail.com>
2024-10-28 15:40:03 +0600

    change resulable component to specific

--- pages/services/diagnostics/index.js
+++ pages/services/diagnostics/index.js
@@ -2,6 +2,11 @@ import Layout from "@/components/Layout";
 import Diagnostics from "@/components/Services/Diagnostics/Diagnostics";
 import CommonInfo from "@/components/Shared/CommonInfo";
 import PageBanner from "@/components/Shared/PageBanner";
+import Container from "@/components/UI/Container";
+import CustomButton from "@/components/UI/CustomButton";
+import SectionTitle from "@/components/UI/SectionTitle";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import diagnosticsImg from "/public/assets/images/animals/cat.jpg";
 
@@ -9,12 +14,38 @@ const DiagnosticsPage = () => {
   return (
     <Layout>
       {/* page banner */}
-      <PageBanner
+      {/* <PageBanner
         headline="diagnostics"
         title="Advanced Diagnostics Mean Accelerated Healing"
         buttonName="Learn More"
         image={diagnosticsImg}
-      />
+      /> */}
+      <div className="tw-bg-tertiary">
+        <Container className="!tw-mt-0 !tw-pr-0 tw-pb-24 tw-pt-7">
+          <div className="tw-flex tw-w-full tw-flex-col tw-justify-between tw-gap-y-10 lg:tw-flex-row lg:tw-items-center lg:tw-gap-x-12">
+            <div className="tw-space-y-3 md:tw-space-y-5 lg:tw-w-1/2 lg:tw-space-y-8 xl:tw-space-y-10">
+              <SectionTitle>Diagnostics</SectionTitle>
+              <p className="tw-text-xl tw-font-medium tw-text-primary md:tw-text-2xl md:tw-font-semibold lg:tw-text-[56px] lg:tw-leading-tight">
+                Advanced Diagnostics Mean Accelerated Healing
+              </p>
+              <div>
+                <Link href="#learn-more">
+                  <CustomButton>Learn More</CustomButton>
+                </Link>
+              </div>
+            </div>
+            <div className="lg:tw-w-1/2">
+              <Image
+                className="tw-h-auto tw-max-w-full tw-rounded-s-full tw-object-cover"
+                src="/assets/images/animals/cat.jpg"
+                alt="animal"
+                width={600}
+                height={400}
+              />
+            </div>
+          </div>
+        </Container>
+      </div>
       {/* information section */}
       <div id="learn-more">
         <CommonInfo
